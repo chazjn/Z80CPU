d91ee61604f846d4c15bf58a90f9f594ed9d7436
Charles Newman <charles.newman@adetiq.co.uk>
2020-06-24 18:54:10 +0100

    Utter lunacy. Committing for reference before scrubbing.

--- Z80CPU/InstructionSet.cs
+++ Z80CPU/InstructionSet.cs
@@ -17,9 +17,71 @@ namespace Z80CPU
 
         public Opcode GetOpcode(IList<byte> bytes)
         {
-            throw new NotImplementedException();
+            if(bytes.Count == 1)
+            {
+                var matched = Opcodes.Where(o => o.Bytes.Count == 1 
+                                              && o.OpcodeParameter.Index == -1 
+                                              && o.Bytes[0] == bytes[0]);
+
+                if(matched.Count() == 1)
+                {
+                    return matched.First();
+                }
+                else
+                {
+                    return null;
+                }
+            }
             
-            
+
+
+
+            if(bytes.Count == 2)
+            {
+                var matched = Opcodes.Where(o => (o.Bytes.Count == 2
+                                              && o.OpcodeParameter.Index == -1
+                                              && o.Bytes[0] == bytes[0]
+                                              && o.Bytes[1] == bytes[1])
+                                              || (o.Bytes.Count == 1
+                                              && o.OpcodeParameter.Index == 1
+                                              && o.Bytes[0] == bytes[0]));
+
+                if(matched.Count() == 1)
+                {
+                    return matched.First();
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+
+            if(bytes.Count == 3)
+            {
+                var matched = Opcodes.Where(o => (o.Bytes.Count == 3
+                                               && o.OpcodeParameter.Index == -1
+                                               && o.Bytes[0] == bytes[0]
+                                               && o.Bytes[1] == bytes[1])
+                                               && o.Bytes[2] == bytes[2]
+                                               || (o.Bytes.Count == 2
+                                               && o.OpcodeParameter.Index == 2
+                                               && o.Bytes[0] == bytes[0]
+                                               && o.Bytes[1] == bytes[1]));
+
+                if (matched.Count() == 1)
+                {
+                    return matched.First();
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+
+
+
         }
     }
 }
