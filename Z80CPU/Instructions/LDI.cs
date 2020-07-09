cd19c89497c32cf85554fa1b2dfa4d9c46e81907
Charles Newman <charles.newman@adetiq.co.uk>
2020-07-09 23:17:30 +0100

    Added LDI Instruction

---
+++ Z80CPU/Instructions/LDI.cs
@@ -0,0 +1,23 @@
+﻿namespace Z80CPU.Instructions
+{
+    public class LDI : Instruction
+    {
+        public LDI()
+        {
+            Opcodes.Add(new Opcode("LDI", 0xED, 0xA0, (z80) =>
+            {
+                var value = z80.Memory.Get(z80.HL.Value);
+                z80.Memory.Set(z80.DE.Value, value);
+
+                z80.DE.Value++;
+                z80.HL.Value++;
+                z80.BC.Value--;
+
+                z80.F.HalfCarry = false;
+                z80.F.ParityOrOverflow = z80.BC.Value == 0 ? false : true;
+                z80.F.Subtraction = false;
+
+            }));
+        }
+    }
+}
