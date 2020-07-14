47b0518f8d546b49f2803fa8d1cfc9b1ef52debf
Charles Newman <charles.newman@adetiq.co.uk>
2020-07-14 22:21:06 +0100

    Added SCF and started PUSH

---
+++ Z80CPU/Instructions/PUSH.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Z80CPU.Registers;
+
+namespace Z80CPU.Instructions
+{
+    public class PUSH : Instruction
+    {
+        public PUSH()
+        {
+            Opcodes.AddRange(new List<Opcode>
+            {
+                new Opcode("PUSH BC", 0xC5, (z80) =>
+                {
+                    Push(z80, z80.BC);
+                }),
+
+                new Opcode("PUSH DE", 0xD5, (z80) =>
+                {
+                    Push(z80, z80.DE);
+                }),
+
+                new Opcode("PUSH HL", 0xE5, (z80) =>
+                {
+                    Push(z80, z80.HL);
+                }),
+
+                new Opcode("PUSH AF", 0xF5, (z80) =>
+                {
+                    Push(z80, z80.AF);
+                }),
+            });
+        }
+
+        private void Push(Z80 z80, Register16 register)
+        {
+            //TODO: Finish this
+            z80.SP.Value--;
+            //var high = z80.Memory.Get()
+        }
+    }
+}
---
+++ Z80CPU/Instructions/SCF.cs
@@ -0,0 +1,15 @@
+﻿namespace Z80CPU.Instructions
+{
+    public class SCF : Instruction
+    {
+        public SCF()
+        {
+            Opcodes.Add(new Opcode("SCF", 0x37, (z80) =>
+            {
+                z80.F.HalfCarry = false;
+                z80.F.Sign = false;
+                z80.F.Carry = true;
+            }));
+        }
+    }
+}
