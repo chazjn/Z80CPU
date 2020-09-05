7b857c63772a605c4c9be67caf6bc5602cf7e3a4
Charles Newman <charles.newman@adetiq.co.uk>
2020-09-05 23:08:37 +0100

    Added EXX instruction

---
+++ Z80CPU/Instructions/EXX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Z80CPU.Instructions
+{
+    public class EXX : Instruction
+    {
+        public EXX()
+        {
+            Opcodes.AddRange(new List<Opcode>
+            {
+                new Opcode("EXX", 0xD9, (z80) =>
+                {
+                    var bc = z80.BC.Value;
+                    var de = z80.DE.Value;
+                    var hl = z80.HL.Value;
+
+                    z80.BC.Value = z80.BC_.Value;
+                    z80.DE.Value = z80.DE_.Value;
+                    z80.HL.Value = z80.HL_.Value;
+
+                    z80.BC_.Value = bc;
+                    z80.DE_.Value = de;
+                    z80.HL_.Value = hl;
+                })
+            });
+        }
+    }
+}
