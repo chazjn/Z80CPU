7d0a28d47140d3dfc332c459ae3f82db21760843
Chaz Newman <chaz.newman@gmail.com>
2021-01-01 19:27:45 +0000

    Started work on ACD and BIT

---
+++ Z80CPU/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Z80CPU
+{
+    public static class Extensions
+    {
+        public static int GetBit(this byte byteValue, int position)
+        {
+            return (byteValue >> position) & 1;
+        }
+
+    }
+}
---
+++ Z80CPU/Instructions/ADC.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Z80CPU.Instructions
+{
+    public class ADC : Instruction
+    {
+        public ADC()
+        {
+            Opcodes.AddRange(new List<Opcode>
+            {
+                new Opcode("ADC A, A", 0x8F, z80 =>
+                {
+                }),
+                
+                new Opcode("ADC A, B", 0x88, z80 =>
+                {
+                }),
+
+                new Opcode("ADC A, C", 0x89, z80 =>
+                {
+                }),
+
+                new Opcode("ADC A, D", 0x8A, z80 =>
+                {
+                }),
+
+                new Opcode("ADC A, E", 0x8B, z80 =>
+                {
+                }),
+
+                new Opcode("ADC A, H", 0x8C, z80 =>
+                {
+                }),
+
+                new Opcode("ADC A, L", 0x8D, z80 =>
+                {
+                }),
+
+                new Opcode("ADC A, n", new[] { new Oprand(0xCE), Oprand.Any }, z80 =>
+                {
+                }),
+
+                new Opcode("ADC A, (HL)", 0x8E, z80 =>
+                {
+                }),
+                
+                new Opcode("ADC A, (IX + d)", new[] { new Oprand(0xDD), new Oprand(0x8E), Oprand.Any }, z80 =>
+                {
+                }),
+
+                new Opcode("ADC A, (IY + d)", new[] { new Oprand(0xFD), new Oprand(0x8E), Oprand.Any }, z80 =>
+                {
+                }),
+            });
+        }
+    }
+}
