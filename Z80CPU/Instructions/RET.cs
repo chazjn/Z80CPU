ebe86bf702de124a731e49d9bbe6329d04988b9f
Chaz Newman <chaz.newman@gmail.com>
2021-01-01 15:06:09 +0000

    Finished implemeting RET

--- Z80CPU/Instructions/RET.cs
+++ Z80CPU/Instructions/RET.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace Z80CPU.Instructions
 {
@@ -14,6 +12,71 @@ namespace Z80CPU.Instructions
                 {
                     Return(z80);
                 }),
+
+                new Opcode("RET NZ", 0xC0, z80 =>
+                {
+                    if(z80.F.Zero == false)
+                    {
+                        Return(z80);
+                    }
+                }),
+
+                new Opcode("RET Z", 0xC8, z80 =>
+                {
+                    if (z80.F.Zero)
+                    {
+                        Return(z80);
+                    }                    
+                }),
+
+                new Opcode("RET NC", 0xD0, z80 =>
+                {
+                    if (z80.F.Carry == false)
+                    {
+                        Return(z80);
+                    }
+                }),
+
+                new Opcode("RET C", 0xD8, z80 =>
+                {
+                   if (z80.F.Carry)
+                    {
+                        Return(z80);
+                    }
+                }),
+
+                new Opcode("RET PO", 0xE0, z80 =>
+                {
+                    if (z80.F.ParityOrOverflow == false)
+                    {
+                        Return(z80);
+                    }
+                }),
+
+                new Opcode("RET PE", 0xE8, z80 =>
+                {
+                    if (z80.F.ParityOrOverflow)
+                    {
+                        Return(z80);
+                    }
+                }),
+
+                new Opcode("RET P", 0xF0, z80 =>
+                {
+                    if (z80.F.Sign == false)
+                    {
+                        Return(z80);
+                    }
+                }),
+
+                new Opcode("RET M", 0xF8, z80 =>
+                {
+                    if (z80.F.Sign)
+                    {
+                        Return(z80);
+                    }
+                }),
+
             });
         }
 
@@ -25,10 +88,8 @@ namespace Z80CPU.Instructions
             z80.PC.Low.Value = low;
             z80.PC.High.Value = high;
 
-            z80.SP.Value = z80.SP.Value++;
-            z80.SP.Value = z80.SP.Value++;
-
-
+            z80.SP.Increment();
+            z80.SP.Increment();
         }
     }
 }
