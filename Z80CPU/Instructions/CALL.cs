1ef151ab4d37db2c53b70d83906f17586ec1d1fe
Chaz Newman <chaz.newman@gmail.com>
2021-01-02 19:57:17 +0000

    Added final CALL opcode

--- Z80CPU/Instructions/CALL.cs
+++ Z80CPU/Instructions/CALL.cs
@@ -9,24 +9,25 @@ namespace Z80CPU.Instructions
             Opcodes.AddRange(new List<Opcode>
             {
                 new Opcode("CALL NZ, pq", 0xC4, (z80) => { Call(z80, z80.F.Zero == false); }),
-                new Opcode("CALL Z, pq",  0xCC, (z80) => { Call(z80, z80.F.Zero == true); }),
+                new Opcode("CALL Z, pq", 0xCC, (z80) => { Call(z80, z80.F.Zero == true); }),
                 new Opcode("CALL NC, pq", 0xD4, (z80) => { Call(z80, z80.F.Carry == false); }),
-                new Opcode("CALL C, pq",  0xE4, (z80) => { Call(z80, z80.F.Carry == true); }),
+                new Opcode("CALL C, pq", 0xE4, (z80) => { Call(z80, z80.F.Carry == true); }),
                 new Opcode("CALL PO, pq", 0xE4, (z80) => { Call(z80, z80.F.ParityOrOverflow == false); }),
                 new Opcode("CALL PE, pq", 0xEC, (z80) => { Call(z80, z80.F.ParityOrOverflow == true); }),
-                new Opcode("CALL P, pq",  0xF4, (z80) => { Call(z80, z80.F.Sign == false); }),
-                new Opcode("CALL M, pq",  0xFC, (z80) => { Call(z80, z80.F.Sign == true); }),
+                new Opcode("CALL P, pq", 0xF4, (z80) => { Call(z80, z80.F.Sign == false); }),
+                new Opcode("CALL M, pq", 0xFC, (z80) => { Call(z80, z80.F.Sign == true); }),
 
+                new Opcode("CALL pq", 0xCD, (z80) => { Call(z80, true); }),
             });
         }
 
-        public void Call(Z80 z80, bool value)
+        public void Call(Z80 z80, bool call)
         {
             //jump over pq bytes
             z80.PC.Increment();
             z80.PC.Increment();
 
-            if (value == true)
+            if (call == true)
             {
                 z80.SP.Decrement();
                 z80.Memory.Set(z80.SP.Value, z80.PC.High.Value);
