894f42f921e00cd03aeaa42c5f6a69a7dba4eb80
Chaz Newman <chaz.newman@gmail.com>
2021-01-02 20:08:43 +0000

    Added CFF

---
+++ Z80CPU/Instructions/CCF.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Z80CPU.Instructions
+{
+    public class CCF : Instruction
+    {
+        public CCF()
+        {
+            Opcodes.AddRange(new List<Opcode>
+            {
+                new Opcode("CCF", 0x3F, (z80) =>
+                {
+                    z80.F.HalfCarry = z80.F.Carry;
+                    z80.F.Subtraction = false;
+                    z80.F.Carry = !z80.F.Carry;
+                })
+            });
+        }
+    }
+}
