3e9e82d7ea96d78eb197b0d92962c7e5ddd025ff
Chaz Newman <chaz.newman@gmail.com>
2021-01-02 22:05:50 +0000

    Added DJNZ

---
+++ Z80CPU/Instructions/DJNZ.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Z80CPU.Instructions
+{
+    public class DJNZ : Instruction
+    {
+        public DJNZ()
+        {
+            Opcodes.AddRange(new List<Opcode>
+            {
+                new Opcode("DJNZ e", 0x10, (z80) => 
+                {
+                    z80.B.Value--;
+
+                    if(z80.B.Value == 0)
+                    {
+                        var offset = z80.Memory.Get(z80.PC.Value);
+                        var pc = z80.PC.Value + (sbyte)offset;
+                        z80.PC.Value = (ushort)pc;
+                    }
+                    else
+                    {
+                        //increment PC to skip the offset
+                        z80.PC.Increment();
+                    }
+                })
+            });
+        }
+    }
+}
