2787f3a488ba0be85a4e22220d890e90dd43e48e
Chaz Newman <chaz.newman@gmail.com>
2021-01-03 22:26:15 +0000

    Refactored ADD but now need tests to check it works correctly. Also need to think about how to implement setting flags across the instructions without duplicate code

--- Z80CPU/Instructions/ADD.cs
+++ Z80CPU/Instructions/ADD.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Z80CPU.Registers;
 
 namespace Z80CPU.Instructions
 {
@@ -10,200 +11,69 @@ namespace Z80CPU.Instructions
             {
                 new Opcode("ADD A, (HL)", 0x86, (z80) => 
                 {
-                    var hl = z80.Memory.Get(z80.HL.Value);
-                    var a = z80.A.Value;
-                    var result = (byte)(hl + a);
-
-                    SetFlags(z80, z80.A.Value, hl, result);
-                    z80.A.Value = result;
+                    var value = z80.Memory.Get(z80.HL.Value);
+                    AddToA(z80, value);
                 }),
                 
-                new Opcode("ADD A, (IX + d)", new[]{ new Oprand(0xDD), new Oprand(0x86), Oprand.Any }, (z80) =>
+                new Opcode("ADD A, (IX + d)", 0xDD, 0x86, (z80) =>
                 {
-                    var offset = z80.Buffer[2];
+                    var offset = z80.GetByte();
                     var ix_offset = z80.IX.Value + offset;
-                    var ix_offset_value = z80.Memory.Get(ix_offset);
-                    var a = z80.A.Value;
-                    var result = (byte)(a + ix_offset_value);
-
-                    SetFlags(z80, z80.A.Value, ix_offset_value, result);
-                    z80.A.Value = result;
+                    var value = z80.Memory.Get(ix_offset);
+                    AddToA(z80, value);
                 }),
                 
-                new Opcode("ADD A, (IY + d)", new[]{ new Oprand(0xFD), new Oprand(0x86), Oprand.Any }, (z80) =>
+                new Opcode("ADD A, (IY + d)", 0xFD, 0x86, (z80) =>
                 {
-                    var offset = z80.Buffer[2];
+                    var offset = z80.GetByte();
                     var iy_offset = z80.IY.Value + offset;
-                    var iy_offset_value = z80.Memory.Get(iy_offset);
-                    var a = z80.A.Value;
-                    var result = (byte)(a + iy_offset_value);
-
-                    SetFlags(z80, z80.A.Value, iy_offset_value, result);
-                    z80.A.Value = result;
+                    var value = z80.Memory.Get(iy_offset);
+                    AddToA(z80, value);
                 }),
                 
-                new Opcode("ADD A, n", new[]{ new Oprand(0xC6), Oprand.Any }, (z80) =>
+                new Opcode("ADD A, n", 0xC6, (z80) => 
                 {
-                    var result = (byte)(z80.A.Value + z80.Buffer[1]);
-
-                    SetFlags(z80, z80.A.Value, z80.Buffer[1], result);
-                    z80.A.Value = result;
-
+                    var value = z80.GetByte();
+                    AddToA(z80, value); 
                 }),
 
-                new Opcode("ADD A, A", 0x87, (z80) =>
-                {
-                    var result = (byte)(z80.A.Value + z80.A.Value);
+                new Opcode("ADD A, A", 0x87, (z80) => { AddToA(z80, z80.A.Value); }),
+                new Opcode("ADD A, B", 0x80, (z80) => { AddToA(z80, z80.B.Value); }),    
+                new Opcode("ADD A, C", 0x81, (z80) => { AddToA(z80, z80.C.Value); }),
+                new Opcode("ADD A, D", 0x82, (z80) => { AddToA(z80, z80.D.Value); }),
+                new Opcode("ADD A, E", 0x83, (z80) => { AddToA(z80, z80.E.Value); }),
+                new Opcode("ADD A, H", 0x84, (z80) => { AddToA(z80, z80.H.Value); }),
+                new Opcode("ADD A, L", 0x85, (z80) => { AddToA(z80, z80.L.Value); }),
 
-                    SetFlags(z80, z80.A.Value, z80.A.Value, result);
-                    z80.A.Value = result;
-                    
-                }),
+                new Opcode("ADD HL, BC", 0x09, (z80) => { AddToRegister(z80, z80.HL, z80.BC.Value); }),
+                new Opcode("ADD HL, DE", 0x19, (z80) => { AddToRegister(z80, z80.HL, z80.DE.Value); }),
+                new Opcode("ADD HL, HL", 0x29, (z80) => { AddToRegister(z80, z80.HL, z80.HL.Value); }),
+                new Opcode("ADD HL, SP", 0x39, (z80) => { AddToRegister(z80, z80.HL, z80.SP.Value); }),
 
-                new Opcode("ADD A, B", 0x80, (z80) =>
-                {
-                    var result = (byte)(z80.A.Value + z80.B.Value);
+                new Opcode("ADD IX, BC", 0xDD, 0x09, (z80) => { AddToRegister(z80, z80.IX, z80.BC.Value); }),
+                new Opcode("ADD IX, DE", 0xDD, 0x19, (z80) => { AddToRegister(z80, z80.IX, z80.DE.Value); }),               
+                new Opcode("ADD IX, IX", 0xDD, 0x29, (z80) => { AddToRegister(z80, z80.IX, z80.IX.Value); }),
+                new Opcode("ADD IX, SP", 0xDD, 0x39, (z80) => { AddToRegister(z80, z80.IX, z80.SP.Value); }),
 
-                    SetFlags(z80, z80.A.Value, z80.B.Value, result);
-                    z80.A.Value = result;
-                }),
-
-                new Opcode("ADD A, C", 0x81, (z80) =>
-                {
-                    var result = (byte)(z80.A.Value + z80.C.Value);
-
-                    SetFlags(z80, z80.A.Value, z80.C.Value, result);
-                    z80.A.Value = result;
-                }),
-
-                new Opcode("ADD A, D", 0x82, (z80) =>
-                {
-                    var result = (byte)(z80.A.Value + z80.D.Value);
-
-                    SetFlags(z80, z80.A.Value, z80.D.Value, result);
-                    z80.A.Value = result;
-                }),
-
-                new Opcode("ADD A, E", 0x83, (z80) =>
-                {
-                    var result = (byte)(z80.A.Value + z80.E.Value);
-
-                    SetFlags(z80, z80.A.Value, z80.E.Value, result);
-                    z80.A.Value = result;
-                }),
-
-                new Opcode("ADD A, H", 0x84, (z80) =>
-                {
-                    var result = (byte)(z80.A.Value + z80.H.Value);
-
-                    SetFlags(z80, z80.A.Value, z80.H.Value, result);
-                    z80.A.Value = result;
-                }),
-
-                new Opcode("ADD A, L", 0x85, (z80) =>
-                {
-                    var result = (byte)(z80.A.Value + z80.L.Value);
-
-                    SetFlags(z80, z80.A.Value, z80.L.Value, result);
-                    z80.A.Value = result;
-                }),
-                
-                new Opcode("ADD HL, BC", 0x09, (z80) => 
-                {
-                    var result = (ushort)(z80.HL.Value + z80.BC.Value);
-
-                    SetFlags(z80, z80.HL.Value, z80.BC.Value, result);
-                    z80.HL.Value = result;
-                }),
-
-                new Opcode("ADD HL, DE", 0x19, (z80) => 
-                {
-                    var result = (ushort)(z80.HL.Value + z80.DE.Value);
-
-                    SetFlags(z80, z80.HL.Value, z80.DE.Value, result);
-                    z80.HL.Value = result;
-                }),
-
-                new Opcode("ADD HL, HL", 0x29, (z80) => 
-                {
-                    var result = (ushort)(z80.HL.Value + z80.HL.Value);
-
-                    SetFlags(z80, z80.HL.Value, z80.HL.Value, result);
-                    z80.HL.Value = result;
-                }),
-
-                new Opcode("ADD HL, SP", 0x39, (z80) => 
-                { 
-                    var result = (ushort)(z80.HL.Value + z80.SP.Value);
-
-                    SetFlags(z80, z80.HL.Value, z80.SP.Value, result);
-                    z80.HL.Value = result;
-                }),
-
-                new Opcode("ADD IX, BC", 0xDD, 0x09, (z80) => 
-                { 
-                    var result = (ushort)(z80.IX.Value + z80.BC.Value);
-
-                    SetFlags(z80, z80.IX.Value, z80.BC.Value, result);
-                    z80.IX.Value = result;
-                }),
-
-                new Opcode("ADD IX, DE", 0xDD, 0x19, (z80) => 
-                {
-                    var result = (ushort)(z80.IX.Value + z80.DE.Value);
-
-                    SetFlags(z80, z80.IX.Value, z80.DE.Value, result);
-                    z80.IX.Value = result;
-                }),
-               
-                new Opcode("ADD IX, IX", 0xDD, 0x29, (z80) => 
-                {
-                    var result = (ushort)(z80.IX.Value + z80.IX.Value);
-
-                    SetFlags(z80, z80.IX.Value, z80.IX.Value, result);
-                    z80.IX.Value = result;
-                }),
-
-                new Opcode("ADD IX, SP", 0xDD, 0x39, (z80) => 
-                {
-                    var result = (ushort)(z80.IX.Value + z80.SP.Value);
-
-                    SetFlags(z80, z80.IX.Value, z80.SP.Value, result);
-                    z80.IX.Value = result;
-                }),
-
-                new Opcode("ADD IY, BC", 0xFD, 0x9, (z80) => 
-                {
-                    var result = (ushort)(z80.IY.Value + z80.BC.Value);
-
-                    SetFlags(z80, z80.IY.Value, z80.BC.Value, result);
-                    z80.IX.Value = result;
-                }),
-
-                new Opcode("ADD IY, DE", 0xFD, 0x19, (z80) => 
-                {
-                    var result = (ushort)(z80.IY.Value + z80.DE.Value);
-
-                    SetFlags(z80, z80.IY.Value, z80.DE.Value, result);
-                    z80.IY.Value = result;
-                }),
-
-                new Opcode("ADD IY, IY", 0xFD, 0x29, (z80) => 
-                {
-                   var result = (ushort)(z80.IY.Value + z80.IY.Value);
-
-                    SetFlags(z80, z80.IY.Value, z80.IY.Value, result);
-                    z80.IY.Value = result;
-                }),
-
-                new Opcode("ADD IY, SP", 0xFD, 0x39, (z80) => 
-                {
-                    var result = (ushort)(z80.IY.Value + z80.SP.Value);
-
-                    SetFlags(z80, z80.IY.Value, z80.SP.Value, result);
-                    z80.IY.Value = result;
-                }),
+                new Opcode("ADD IY, BC", 0xFD, 0x09, (z80) => { AddToRegister(z80, z80.IY, z80.BC.Value); }),
+                new Opcode("ADD IY, DE", 0xFD, 0x19, (z80) => { AddToRegister(z80, z80.IY, z80.DE.Value); }),
+                new Opcode("ADD IY, IY", 0xFD, 0x29, (z80) => { AddToRegister(z80, z80.IY, z80.IY.Value); }),
+                new Opcode("ADD IY, SP", 0xFD, 0x39, (z80) => { AddToRegister(z80, z80.IY, z80.SP.Value); })
             });
+        }
+
+        public void AddToA(Z80 z80, byte value)
+        {
+            var result = (byte)(z80.A.Value + value);
+            z80.A.Value = result;
+            SetFlags(z80, z80.A.Value, value, result);
+        }
+
+        public void AddToRegister(Z80 z80, Register16 destination, ushort value)
+        {
+            var result = (ushort)(destination.Value + value);
+            destination.Value = result;
+            SetFlags(z80, destination.Value, value, result);
         }
 
         private void SetFlags(Z80 z80, byte original, byte addition, byte result)
